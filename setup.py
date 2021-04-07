964a7c19b955f2bc52d788e025eda61750010cb4
Alex Tong <alexanderytong@gmail.com>
2021-04-07 16:36:01 -0400

    Update setup with requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import find_packages, setup
 
+install_requires = [
+    "POT",
+    "sklearn",
+    "scipy",
+    "numpy",
+    "torch",
+    "git+https://github.com/atong01/unbalanced_gromov_wasserstein",
+]
+
 setup(
     name="SCOT",
     version="0.2.1",
@@ -7,4 +16,5 @@ setup(
     author="Pinar Demetci",
     packages=find_packages(),
     license="MIT",
+    install_requires=install_requires,
 )
